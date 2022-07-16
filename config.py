869eedffa59e311fd8c866dbc8efef0162e6bbd9
0xSn1kky <88375726+0xSn1kky@users.noreply.github.com>
2022-07-17 00:55:39 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # Сюда вставить токен бота
-token = "OTk3OTA5MzUxMjExNzQ1MzUw.GLcjNg.QtmQwwWKGgtOwQAu3sRQj2YCU1-Ik_d5w1GUlc"
+token = "TOKEN"
 # Сюда вставьте имя бота
-botname = "PinkyUPBot"
+botname = "BOT NAME"
